1d9a0bebf76f3f6a71fa49e2f648f6e59dfaab9a
PedroCafe-cwb <githubcafe@gmail.com>
2026-09-09 11:03:34 -0300

    ok

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMcontentLoaded", ()=>{
+    constant likeBtn = document.querySelector(".like-btn");
+    const postMidia = document.querySelector(".posta-media");
+    if (!LikeBtn) return;
+
+    const likesContSpan = likeBtn.querySelector(".like-count");
+    const bookmarkBtn = document.querySelector(".bookmark-btn");
+    
+    let isLiked = false;
+    let baseLikes = 0;
+    
+
+})
